5ca670abe8d339d8ed27affcde5bd39c2ef18434
Richmond Ko <richmond.ko@outlook.ph>
2018-04-11 18:31:01 +0800

    Layout detail view of character

---
+++ RickAndMortyInfo/RickAndMortyInfo/Utility/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  RickAndMortyInfo
+//
+//  Created by Richmond Ko on 11/04/2018.
+//  Copyright © 2018 Richmond Ko. All rights reserved.
+//
+
+import Foundation
+
+enum SegueIdentifier: String {
+    case showCharacterDetail = "showCharacterDetail"
+}
